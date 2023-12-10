1a2910f02aa5cc2314c82c470e05b4cb512239d2
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-10 06:29:15 +0000

    chlo

---
+++ 01_basics/operation.js
@@ -0,0 +1,39 @@
+// let value = 3
+// let negValue = -value
+//console.log(negValue);
+
+// console.log(2+2)
+// console.log(2-2)
+// console.log(2*2)
+// console.log(2**3)
+// console.log(2/3)
+// console.log(2%3)
+
+// let str1 = "ravi"
+// let str2 = "kumar"
+
+// let str3 = str1+str2
+// let str4 = str1-str2 //it will gives "NaN" because substraction not occur in Javascript for "string"
+// let str5 = str2-str1 //it will gives "NaN" because substraction not occur in Javascript for "string"
+
+// console.log(str3)
+// console.log(str4)
+// console.log(str5)
+
+
+// let x = 1+"2"
+// let x2 = "1"+2
+
+// console.log(x)
+// console.log(x2)
+// console.log(typeof x)
+// console.log(typeof x2)
+
+// console.log("1" + 2 + 2) // it will gives us 122 as string
+// console.log(1 + 2 + "2") // it will gives us 122 as string 32
+ 
+console.log(true)  // it will gives true 
+console.log(+true) // it will gives 1 as output --> lekin aise likhna nhi hai
+console.log(true+); // it will gives error as output --> lekin aise likhna nhi hai
+console.log(+"");   // it will gives 0 as output --> lekin aise likhna nhi hai
+
