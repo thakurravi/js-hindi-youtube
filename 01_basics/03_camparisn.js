8100a269878bd3e5871023cb378cb53e70e9f270
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-10 09:52:37 +0000

    hoo

---
+++ 01_basics/03_camparisn.js
@@ -0,0 +1,27 @@
+// console.log(2>1)
+// console.log(2>=1)
+// console.log(2<1)
+// console.log(2==1)
+// console.log(2!=1)
+
+
+// console.log("2">1); // it will gives "true" as output 
+// console.log("02">1); // it will gives "true" as output 
+
+// console.log(null>0)
+// console.log(null==0)
+// console.log(null>=0)
+// console.log(null<=0)
+// console.log(null<0)
+
+
+// console.log(undefined<0)
+// console.log(undefined>0)
+// console.log(undefined==0)
+// console.log(undefined<=0)
+// console.log(undefined>=0)
+
+
+// === triple check is called strict check
+
+// console.log("2"===2) // it will gives false because it only check if two data types is same
