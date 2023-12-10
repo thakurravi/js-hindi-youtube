3ed3d25ab9aad1f745f9d60ed156886021821f4e
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-10 15:00:20 +0000

    ok

---
+++ 01_basics/05_mathsAndNums.js
@@ -0,0 +1,55 @@
+// const score = 400
+// console.log(score)
+// const balance = new Number(100)
+// // console.log(balance)
+// console.log(balance.toString())
+// const newString = balance.toString();
+// console.log(newString);
+// console.log(newString.length)
+
+// console.log(balance.toFixed(2));  // it will gives 100.00 as output 
+
+// const otherNumber = 23.8966
+// console.log(otherNumber.toPrecision(3));  
+
+// const otherNumber2 = 123.8966
+// console.log(otherNumber2.toPrecision(3));   
+
+
+// const otherNumber3 = 1123.8966
+// console.log(otherNumber3.toPrecision(3));   
+
+// const hundreds = 1000000
+// console.log(hundreds.toLocaleString());
+
+
+// const hundreds2 = 1000000
+// console.log(hundreds2.toLocaleString('en-IN'));
+
+
+//++++++++++++++++++++++++++++Maths++++++++++++++++++++++++++++++++++++++
+
+
+console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(8.9));
+// console.log(Math.round(2.3));
+// console.log(Math.round(-4.3));
+// console.log(Math.round(-4.9));
+// console.log(Math.ceil(4.1));
+// console.log(Math.floor(4.9));
+// console.log(Math.sqrt(25));
+// console.log(Math.min(4,3,6,8));
+// console.log(Math.max(4,3,6,8));
+console.log(Math.random());
+console.log((Math.random()*10) + 1);
+console.log(Math.floor(Math.random()*10) + 1);
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random()*(max-min+1))+min)
+
+
+
+
