c7ad0eeac39be3751113efc256a65221b055d7a1
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-10 13:15:44 +0000

    chlo

---
+++ 01_basics/04_string.js
@@ -0,0 +1,49 @@
+const name = "hitesh"
+const repoCount = 50
+
+// console.log(name + repoCount + " value")  
+
+// console.log(`Hello my name is ${name} and my name is ${repoCount}`);
+
+const gameName = new String('hitesh-hc-com')
+// console.log(gameName);
+// console.log(gameName[4]);
+// console.log(gameName.__proto__);
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('t'));
+
+// const newString = gameName.substring(0, 4)
+// console.log(newString);
+
+// const anotherString = gameName.slice(-8, 4)
+// console.log(anotherString);
+
+// const newStringOne =  "    hitesh    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "https://hitesh.com/hitesh%20chaudhary"
+// console.log(url.replace('%20', '-'))
+
+// console.log(url.includes('hitesh'));
+// console.log(url.includes('sundar'));
+
+
+console.log(gameName.split('-'))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
