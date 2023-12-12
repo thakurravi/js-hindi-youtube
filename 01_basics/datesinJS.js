d7e3a5ad8a7ed57929fe3811a117ccb3659f031e
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-12 15:54:55 +0000

    ok

---
+++ 01_basics/datesinJS.js
@@ -0,0 +1,37 @@
+// Dates
+
+
+let myDate = new Date()
+// console.log(myDate);
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(typeof myDate);
+
+// let myCreatedDate = new Date(2023,0,23)
+// let myCreatedDate = new Date(2023,0,23,5,3)
+// let myCreatedDate = new Date("2023-12-14")
+let myCreatedDate = new Date("01-14-2023")
+// console.log(myCreatedDate);
+// console.log(myCreatedDate.toLocaleString());
+// console.log(myCreatedDate.getTime());  for finding or calculating time in milliseconds
+
+
+let myTimeSpam = Date.now()
+// console.log(myTimeSpam) in millisecond
+// console.log(myTimeSpam)in millisecond
+// console.log(Date.now())
+// console.log(Date.now()/1000);
+// console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+// console.log(newDate);
+// console.log(newDate.getMonth()+1);
+// console.log(newDate.getDay());
+
+/* for more customization
+newDate.toLocaleString ('default', {
+    weekday : "long",
+
+})
+*/
