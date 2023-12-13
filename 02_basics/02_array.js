daa562e6f636d2f0a1691bcde451db58f37fe009
Ravi Kumar <76400555+thakurravi@users.noreply.github.com>
2023-12-13 11:02:01 +0000

    oho

---
+++ 02_basics/02_array.js
@@ -0,0 +1,36 @@
+const marvel_heros = ["Thor", "Spiderman", "Flash"]
+const dc_heros = ["Superman", "batman", "Thanos"]
+
+
+// const new_heros1 = marvel_heros.push(dc_heros)
+// console.log(new_heros1);  // it gives 4 as output
+// console.log(marvel_heros);
+
+
+// console.log(marvel_heros.concat(dc_heros));
+// console.log(marvel_heros);
+
+// const new_heros2 = marvel_heros.concat(dc_heros)
+// console.log(new_heros2);
+// console.log(marvel_heros);
+
+
+// const all_new_heros = [...marvel_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7,[4,5]]]
+// const real_another_array = another_array.flat(Infinity);
+// console.log(real_another_array);
+
+
+// console.log(Array.isArray("Ravi"))
+// console.log(Array.from("Ravi"))
+// console.log(Array.from({name: "Ravi"})) // it gives a empty array []
+
+
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+console.log(Array.of(score1,score2,score3));
